4710626126fc6c4d175723ab0c0f3b7a97c1721b
gus <gustavosrod1121@gmail.com>
2026-04-28 22:27:00 -0700

    Create login system

--- server.js
+++ server.js
@@ -4,12 +4,39 @@ const PORT = 3000;
 
 app.use(express.static("public"));
 
+
+
+app.use(express.urlencoded());
+const usersArr = [
+  { username: "admin1", password: "admin1" },
+  { username: "admin2", password: "admin2" },
+  { username: "user1", password: "password1" },
+  { username: "user2", password: "password2" },
+  { username: "user3", password: "password3" }
+];
+
+app.post("/login", (req, res) => {
+    const userFound = usersArr.find(
+        element => element.username == req.body.username 
+        && element.password == req.body.password
+    );
+
+    if (userFound) {
+        res.send(`Welcome ${req.body.username}`);
+    } else {
+        res.status(401)
+        .send("Bad attempt. You are not going to get any of this soup!");
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 /* Send a 204 No Content Found when the browser asks for a tab icon */
 app.get("/favicon.ico", (req, res) => res.status(204).end());
+
 
 app.get("/", (req, res) => {
     res.redirect("/home");
